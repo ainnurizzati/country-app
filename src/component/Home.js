7a8a867a5ac5e2bee56859ac50e0a24ce4fcd4cd
Ain Nurizzati <ainnurizzati24@gmail.com>
2022-08-10 01:19:17 +0800

    Initial commit

---
+++ src/component/Home.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import styles from '../component/Home.css';
+
+function Home() {
+
+    return(
+       <html>
+            <head> 
+            </head>
+            <body>
+                <p>Hello</p>
+            </body>
+       </html>
+    );
+}
+
+export default Home;
